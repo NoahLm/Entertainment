d037058f7661a21ac7dbc7c38d319b2a1c37980d
Noe Lozano Mendoza <nolom2012@gmail.com>
2022-05-09 21:12:13 -0500

    Return added to read function

--- PythonProjects/Hangman/read_write_files.py
+++ PythonProjects/Hangman/read_write_files.py
@@ -2,6 +2,6 @@ def read_file():
     with open("./DataFiles/data.txt", "r", encoding='utf-8') as f:
         words = [word.rstrip("\n") for word in f]
     
-    print(words)
+    return words
 
 ###read_file()
