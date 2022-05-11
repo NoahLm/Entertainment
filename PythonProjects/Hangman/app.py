c4ff699e86ab983aa99eea3b6823a66707dc20e8
Noe Lozano Mendoza <nolom2012@gmail.com>
2022-05-10 23:43:37 -0500

    Comments were added to app.py

--- PythonProjects/Hangman/app.py
+++ PythonProjects/Hangman/app.py
@@ -4,6 +4,7 @@ import read_write_files as rw
 
 import random
 
+#This function searches for the user letter in the word. Return True if it is there, and False otherwise.
 def letter_check(letter , word, support_list, lives):
     for i in range(len(word)):
         if letter == word[i]:
@@ -19,16 +20,21 @@ def letter_check(letter , word, support_list, lives):
     print("U did not found a correct letter")
     return False
 
+#From the list of words extracted from the file, returns one of them randomly
 def choosing_random_word(file_words):
     index = random.randint(0, len(file_words))
     return file_words[index]
 
+#Creates a support list, the list will register which letters have been found by changing its values
+#from 0 to 1. 1 if it was found, 0 if not. 
 def creating_support_list(word):
     return [0 for i in range(len(word))]
 
+#Useful for selecting the difficulty of the game... Amount of lives 
 def difficulty():
     pass
 
+#The whole process of the game is going to be here. 
 def game(difficulty_lives):
     word = choosing_random_word(rw.read_file())
     supp_list = creating_support_list(word)
@@ -37,8 +43,6 @@ def game(difficulty_lives):
 
     while lives >= 0:
         pass
-
-
 
 def main(): 
     ui.clear_scrn()
